9921d1e530cf684e3779bfd76b6cffbc5a8dea57
Sally <jw4074@columbia.edu>
2025-04-20 11:13:24 -0700

    We can no longer have evil tags by our pre-condition, yay

--- src/coupon_collector/SimpleCouponCollector.lean
+++ src/coupon_collector/SimpleCouponCollector.lean
@@ -5,10 +5,17 @@
 /-
 To discuss w/ Keith: 
 0. --are the following definitions of truth shaky? 
-   - true tag inputs (only tags created by apply_coupon_collector are true) 
+    - true tag inputs (only tags created by apply_coupon_collector AND valid requests are true) 
       a valid identity request & uint8 x;
       a valid symmety rerquest & t₁(x, y) from apply_coupon_collector; - enforcing x ≡ y or not? 
       a valid transitivity request & t₁(x, y), t₂(y,z) from apply_coupon_collector;
+
+    - Alternatively, if we we want to simplify things, we just define true tags as tags created by 
+  apply_coupon_collector function only and smiply use true tags as a pre-condition
+
+  We prove pre/post conditions hold in two theorems due two slightly different pre-conditions as
+  written above. 
+
    - true tag outputs: 
       if (a valid identity request & uint8 x) => t(x, x);
       if (a valid symmety rerquest & t₁(x, y)) => t₁'(y, x);
@@ -52,12 +59,12 @@ def Post (x y z : Type) : Type := x → y → z → Prop
 /-------------------------------------------------------------------- 
 valid/true inputs (tags) and outputs (tags) below
 
-pre: all tags input are true; 
-post: all tags output are true; 
+pre: all tag inputs are true; 
+post: all tag outputs are true; 
 ----------------------------------------------------------------------/
-def valid_request : Request → Prop
+def valid_request : Request → Prop --helper definition used in pre_condition later on 
 | Request.Identity _ => True
-| Request.Symmetry _ => True   -- any tag is fine for now, because true_tag will enforce validity
+| Request.Symmetry _ => True   -- any tag is fine for now, because we will enforce tag validity in pre
 | Request.Transitivity (t₁, t₂) => t₁.rhs = t₂.lhs
 
 
@@ -76,10 +83,8 @@ def post_condition : Post Request Tag UInt8 :=
     match r with
     | Request.Identity y =>
         x = y ∧ t.lhs = y ∧ t.rhs = y
-
     | Request.Symmetry t₁ =>
         t.lhs = t₁.rhs ∧ t.rhs = t₁.lhs
-
     | Request.Transitivity (t₁, t₂) =>
         t₁.rhs = t₂.lhs ∧
         t.lhs = t₁.lhs ∧
@@ -137,10 +142,11 @@ by
     -- applied : apply_coupon_collector (Transitivity (t₁, t₂)) = some t
 
     -- Instead of evaluating apply_coupon_collector, we define what it must return
-    -- and use the injectivity of `Option.some`
+    -- and use the injectivity of `Option.some` 
     have h :
       apply_coupon_collector (Request.Transitivity (t₁, t₂)) = some expected := by
         simp [apply_coupon_collector, valid]
+        --rfl -- try this out: it's a very frequent bug we'll encounter in Lean4 
         exact ⟨valid, rfl⟩ -- learned lesson: this line solves a very pesky bug on shape mismatch 
 
     rw [h] at applied
@@ -150,105 +156,12 @@ by
     exact ⟨Request.Transitivity (t₁, t₂), t₁.lhs, ⟨valid, ⟨rfl, rfl⟩⟩⟩
 
 
-
-
-
-
-
-
--- we can do case match for validRequest, becase it is one-to-one mapping: request to prop
-class ValidRequest (r : request) : Prop where -- valid requests are a type class 
-  ok : match r with -- ok is a proof itself: 
-                    -- in order to be considered a ValidRequest r, 
-                    -- you must be able to prove the condition given by the match expression for r
-      -- matching identity to a True prop
-       | request.Identity _ => True
-       -- matching symmetry to a True prop
-       | request.Symmetry _ => True
-       -- t₁.rhs = t₂.lhs below  is saying this: Eq (t₁.rhs) (t₂.lhs) : Prop
-       | request.Transitivity (t₁, t₂) => t₁.rhs = t₂.lhs
-
--- we choose to use instance of class type for output because: 
--- t->t is not a unique relation. It's a one-to-many; 
--- A single request type can map to many tag values
--- Also, output maps request->tag->prop: if we use match here, the shape of prop 
--- might lead to very complex proof to ensure type match
-class ValidOutput (r : request) (t : Tag) : Prop
-
-
-/-----------------------------------------------------------------
-Define valid output instances to prepare theorem prooving
-
-From output type, we define valid output instances below. It's saying
-
-"Whenever you see this exact shape of r and t, the proposition ValidOutput r t holds"
-------------------------------------------------------------------/
--- Identity: must produce (x, x)
-instance validOutput_identity (x : UInt8) :
-  ValidOutput (request.Identity x) { lhs := x, rhs := x } :=
-  ⟨⟩
-
--- Symmetry: must produce (y, x) from (x, y)
-instance validOutput_symmetry (t : Tag) :
-  ValidOutput (request.Symmetry t) { lhs := t.rhs, rhs := t.lhs } :=
-  ⟨⟩
-
--- Transitivity: must produce (x, z) from (x, y), (y, z)
-instance validOutput_transitivity (t₁ t₂ : Tag) (h : t₁.rhs = t₂.lhs) :
-  ValidOutput (request.Transitivity (t₁, t₂)) { lhs := t₁.lhs, rhs := t₂.rhs } := 
-  ⟨⟩
-
-/------------------------------------------------------------------------
-Functional correctness (soundness) proof of apply_coupon_collector: 
-
-For every request r that is a valid input (ValidRequest r),
-there exists a tag t such that:
-
-  1. totality corect: apply_coupon_collector to valid r, it returns some t
-
-  2. semantically correct: t is a correct output for that input
-----------------------------------------------------------------------------/
-theorem apply_coupon_collector_functionally_correct
-  (r : request) [ValidRequest r] :
-  ∃ t : Tag,
-  apply_coupon_collector r = some t ∧ ValidOutput r t :=
-by
-  cases r with
-  | Identity x =>
-      -- we can technically simply these apply stuff by constructor tactic (requiring more debugging than Apply)
-      apply Exists.intro { lhs := x, rhs := x } --matchs ∃ in proof statement above 
-      apply And.intro -- matches ⋀ in proof statement 
-      simp [apply_coupon_collector]
-      exact validOutput_identity x  
-
-  | Symmetry t₀ =>
-   apply Exists.intro { lhs := t₀.rhs, rhs := t₀.lhs }
-   apply And.intro
-   simp [apply_coupon_collector]
-   exact validOutput_symmetry t₀
-  -- Lean 4 does not allow pattern matching with 
-  -- parentheses like (t₁, t₂) in | ConstructorName pattern 
-  -- We should avoid Transitivity (t₁, t₂) here
-  -- we should avoid using have h: ValidRequest.ok due to terrible type mismatch
-  | Transitivity pair =>
-      apply Exists.intro { lhs := pair.fst.lhs, rhs := pair.snd.rhs }
-      apply And.intro
-      simp [apply_coupon_collector]
-      -- we need the following line to help Lean deduct the right case, 
-      -- because we cannot pass Transitivity t₁ t₂ to the cases r match 
-      exact ValidRequest.ok (r := request.Transitivity pair)
-      exact validOutput_transitivity pair.fst pair.snd (ValidRequest.ok (r := request.Transitivity pair))
-  
-
-/-
-Test cases, test theorems for coupon collector below 
--/
-
-#eval apply_coupon_collector (request.Identity 5)
-#eval apply_coupon_collector (request.Symmetry { lhs := 2, rhs := 50 })
--- some { lhs := 5, rhs := 5 }
+/-----------------
+Some tests below: 
+------------------/
+-- We can write arbitrary code in lean, but it will not satisfy the pre-conditions 
 def evilTag (x : UInt8) : Tag := { lhs := x, rhs := 50 }
 
-
-
-
+-- now the evilTag will not create a tag satisfying either of our pre-conditions
+true_tag (evilTag 5) 
+pre_condition r (evilTag x) x
