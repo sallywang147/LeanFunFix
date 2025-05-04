3fe8e7309d4693306ff2545020bdf534ea736cfa
Sally <jw4074@columbia.edu>
2025-05-03 17:28:44 -0700

    revise KVStore to a tree of keys and test functions

--- src/coupon_collector/ExperimentCouponCollector.lean
+++ src/coupon_collector/ExperimentCouponCollector.lean
@@ -1,27 +1,46 @@
-import Init.Data.String.Basic
+--import Paperproof
 
--- import Paperproof
-
-/-- 
-Next steps from Hackathon discussion: 
-  String->Value: 
-    1. Value can be Nat; 
-    2. Value can be a tree (an array of strings/keys)
---/
-
--- each key is String type for now
-
-
-
-structure Tag where
+structure Tag where 
   lhs : String 
   rhs : String
 deriving Repr, BEq, DecidableEq
 
 
-structure KVStore where 
-  mapping : String → Option Nat
+/-- 
+Next steps from Hackathon discussion: 
+  String->Value: 
+    1. Value can be a Nat; 
+    2. Value can be a tree (an array of strings/keys)
+    
+structure KVStore where
+  mapping : String → Option (Sum Nat (Array String))
 
+--/ 
+
+structure KVStore where -- key can point to a value or a tree (string array)
+  mapping : String → Option (Nat ⊕ Array String)
+ 
+
+def newStore : KVStore :=
+  { mapping := λ key =>
+      if key == "a" then some (Sum.inl 42)
+      else if key == "b" then some (Sum.inr #["c", "d"])
+      else if key == "c" then some (Sum.inl 42)
+      else if key == "d" then some (Sum.inl 5)
+      else none
+  }
+
+-- some test functions 
+
+def getValue (store : KVStore) (key : String) : 
+  Option (Nat ⊕ Array String) :=
+  store.mapping key
+
+
+#eval getValue newStore "a"       -- Output: some 42
+#eval getValue newStore  "b" 
+#eval getValue newStore  "c" 
+#eval getValue newStore  "d" 
 -- same request,apply_coupon_collector functions below: 
  inductive Request --exposed to user
  | Identity : String → Request
@@ -49,53 +68,4 @@ theorem soundness_proof :
     all_tag_true store (Option.toList (apply_coupon_collector r)) := by
   intros r store h 
   cases r with 
-    | Identity x => 
-      simp [Request.toList, Option.toList, all_tag_true, apply_coupon_collector]
-      
-    | Symmetry t =>
-      simp [Request.toList, all_tag_true] at h
-      simp [apply_coupon_collector, Option.toList, all_tag_true]
-      -- h : (store.mapping t.lhs == store.mapping t.rhs) = true
-      -- Goal: (store.mapping t.rhs == store.mapping t.lhs) = true
-
-      cases h₁ : store.mapping t.lhs with
-      | none =>
-        cases h₂ : store.mapping t.rhs with
-        | none =>
-          simp [h₁, h₂]
-        | some v₁ =>
-          simp [h₁, h₂] at h
-
-      | some v₂ =>
-        cases h₂ : store.mapping t.rhs with
-        | none =>
-          simp [h₁, h₂] at h
-
-        | some v₁ =>
-          simp [h₁, h₂] at h
-          -- h : (some a == some b) = true ⇒ a = b
-          have : v₂ = v₁ := by
-            cases h
-            rfl
-          simp [h₁, h₂, this.symm]
-      
-    | Transitivity t₁ t₂ =>
-        simp [Request.toList, all_tag_true] at h
-        cases h with
-        | intro h₁ h₂ =>
-          cases cond : t₁.rhs == t₂.lhs               
-          case false =>
-            simp [apply_coupon_collector, cond, Option.toList, all_tag_true]
     
-          case true =>
-            simp [apply_coupon_collector, cond, Option.toList, all_tag_true]
-            apply List.forall_mem_singleton.2
-            simp at h₁ h₂
-            have cond_eq : t₁.rhs = t₂.lhs := of_decide_eq_true cond
-            have map_eq : store.mapping t₁.rhs = store.mapping t₂.lhs :=
-                congrArg store.mapping cond_eq
-            have h₂' : store.mapping t₁.rhs = store.mapping t₂.rhs :=
-               map_eq.trans h₂
-            exact h₁.trans h₂'
-            simp 
-         
